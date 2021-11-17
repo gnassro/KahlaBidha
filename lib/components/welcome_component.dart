30b67e61669d966c4942f5e8cfb8fa4494229190
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-17 19:28:19 +0100

    Welcome widget

--- lib/components/welcome_component.dart
+++ lib/components/welcome_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kahla_bidha_login_page/library/colors.dart' as global_colors;
 
 class WelcomeComponent extends StatelessWidget {
@@ -10,13 +11,10 @@ class WelcomeComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       shrinkWrap: true,
-      children: const [
+      children: [
         Text('Kahla Bidha',
             textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 200.0,
-                fontFamily: 'Praise'
-            )
+            style: GoogleFonts.pacifico()
         ),
       ],
     );
