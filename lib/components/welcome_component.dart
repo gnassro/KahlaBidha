541ffdc0b32d9cca87e862fd3087e276b6942b6a
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-17 22:27:14 +0100

    AutoText Size

--- lib/components/welcome_component.dart
+++ lib/components/welcome_component.dart
@@ -24,6 +24,7 @@ class WelcomeComponent extends StatelessWidget {
                 maxFontSize: 120.0,
                 style: GoogleFonts.pacifico(
                     fontSize: 180.0,
+                    height: 3.0,
                     color: global_colors.whiteColor,
                 )
             ),
