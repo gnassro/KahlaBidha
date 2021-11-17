a99d032ef3e53ce518b5097edf19936a0f67744e
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-17 20:46:57 +0100

    AutoText Size

--- lib/components/welcome_component.dart
+++ lib/components/welcome_component.dart
@@ -18,6 +18,7 @@ class WelcomeComponent extends StatelessWidget {
           child: AutoSizeText('Kahla Bidha',
               textAlign: TextAlign.center,
               maxLines: 2,
+              minFontSize: 80.0,
               style: GoogleFonts.pacifico(
                   fontSize: 140.0,
                   color: global_colors.whiteColor,
