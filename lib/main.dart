b35579e8cfa729baf4084cc8abfa411584153ccb
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-18 18:01:51 +0100

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
+        future: Init.instance.initialize(),
         builder: (context, AsyncSnapshot snapshot) {
           // Show splash screen while waiting for app resources to load:
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -49,5 +50,13 @@ class AppComponent extends StatelessWidget {
       home: const LoginComponent(),
     );
   }
+}
 
+class Init {
+  Init._();
+  static final instance = Init._();
+
+  Future initialize() async {
+    await Future.delayed(const Duration(seconds: 1));
+  }
 }
