060e1ddd70c8c3b73f62d7d82ca1b7fc3c9d9363
gnassro <38044719+gnassro@users.noreply.github.com>
2021-11-18 23:47:06 +0100

    ready for release

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kahla_bidha_login_page/pages/login.dart';
-//import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
+import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 import 'package:kahla_bidha_login_page/library/colors.dart' as global_colors;
 import 'package:kahla_bidha_login_page/scroll_behavior.dart';
 import 'package:flutter/services.dart';
 import 'package:kahla_bidha_login_page/pages/splash_screen.dart';
 
 void main() {
-  //TODO uncomment it when release
-  //configureApp();
+  configureApp();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
